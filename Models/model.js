a5be54110d3a6a6c70175c5a83fab3d317fe4c22
Edwin Ibarra <auxiliaredesarrollo1@agaval.com.co>
2022-05-10 19:58:25 -0500

    Detalles minimos

--- Models/model.js
+++ Models/model.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose')
 const schema = require('./schemas')
 const uri = "mongodb+srv://IngenieriaWeb2:fBfIW7ivJEebigme@cluster0.xx0gm.mongodb.net/IngenieriaWeb2?retryWrites=true&w=majority";
 const Schema = mongoose.Schema
-let usuarioConect;
+//let usuarioConect;
 
 class Schemas {
     usuarioConect = null
     constructor(){
         this.usuarios()
-        mongoose.connect(uri)
+    }
+    async conectar(){
+        await mongoose.connect(uri)
+    }
+    desconectar(){
+        mongoose.disconnect()
     }
     usuarios(){
         let usuario = new Schema(schema.usuarios,{collection:'usuarios'})
